d128fe42def53df5789be871a8b1f70ba183a752
Aleksandra Jensen <alekmisjen@gmail.com>
2025-01-22 13:32:38 +0100

    showAll button shows all the events, and filter the date added

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const eventDateInput = document.querySelector("#eventDate");
 const eventDescriptionInput = document.querySelector("#eventDescription");
 const addButton = document.querySelector("#addEventButton");
 const eventsList = document.querySelector("#eventsList");
-const showAllDaysButton = document.querySelector("#showAllDays");
+const showAll = document.querySelector("#showAllEvents");
+const daysButtons = document.querySelector(".btn-group");
 
 // 1. Function to add a new event
 function addEvent() {
@@ -26,10 +27,10 @@ function addEvent() {
 }
 //3. Function to render all  event
 // Function to render events
-function renderEvents() {
+function renderEvents(filteredEvents = null) {
   // Clear the current list
   eventsList.innerHTML = "";
-  const eventsToRender = events;
+  const eventsToRender = filteredEvents || events;
 
   // Add each event as a list item
   eventsToRender.forEach((event) => {
@@ -45,7 +46,30 @@ function renderEvents() {
     listItem.appendChild(removeButton);
   });
 }
+//function to generate the next seven days buttons
+function generateNextSevenDays() {
+  for (let i = 0; i < 7; i++) {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + i);
 
+    const formattedDate = currentDate.toISOString().split("T")[0];
+
+    const button = document.createElement("button");
+    button.type = "button";
+    button.className = "btn btn-secondary";
+    button.textContent = formattedDate;
+    button.addEventListener("click", (e) => filterEventsByDay(formattedDate));
+    daysButtons.appendChild(button);
+  }
+}
+generateNextSevenDays();
+
+function filterEventsByDay(selectedDate, currentBtn) {
+  const filteredEvents = events.filter((event) => event.date === selectedDate);
+  renderEvents(filteredEvents);
+}
+
+showAll.addEventListener("click", () => renderEvents(events));
 //2. event listener for the addEvent button
 addButton.addEventListener("click", (e) => {
   e.preventDefault();
